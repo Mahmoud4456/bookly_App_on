06d1be32f37b7aaaa6192ced7bbb7d957fdb653e
Mahmoud4456 <bnyadm2213@gmail.com>
2024-08-18 14:40:22 +0300

    create styles font

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
-abstract class Styles{
-  static const textTitle = TextStyle(
+abstract class Styles {
+  static const textTitle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textTitle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textTitle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const textTitle20 = TextStyle(
     fontSize: 20,
-    fontWeight: FontWeight.w600 ,
-  ) ;
+    fontWeight: FontWeight.normal, // font family
+  );
+  static const textTitle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal, // font family
+  );
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 
+import 'package:demo/core/utils/assets.dart';
 import 'package:demo/core/utils/styles.dart';
 import 'package:demo/features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:demo/features/home/presentation/views/widgets/feature_home_List_view.dart';
@@ -17,13 +18,46 @@ class HomeViewBody extends StatelessWidget {
         CustomListView(),
         SizedBox(height: 50,),
         Padding(
-          padding: EdgeInsets.only(left: 18),
-          child: Text("Best Seller", style: Styles.textTitle,),
+          padding: EdgeInsets.only(left: 18 , bottom: 15),
+          child: Text("Best Seller", style: Styles.textTitle18,),
         ),
+        BestSellerListViewItems(),
       ],
     );
   }
 }
+
+class BestSellerListViewItems extends StatelessWidget {
+  const BestSellerListViewItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+        AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.green,
+            image: const DecorationImage(
+                image: AssetImage(assetsData.imageTest), fit: BoxFit.fill),
+          ),
+        ),
+      ),
+          const Column(
+            children: [
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 
 
