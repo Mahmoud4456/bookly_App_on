348b96264b7e48df0ab8bad1f94739f6237c2d5e
Mahmoud4456 <bnyadm2213@gmail.com>
2024-09-03 16:36:51 +0300

    implement remote feature fetch data

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 
-class Api{
+class ApiService{
 
   final String baseUrl = "https://www.googleapis.com/books/v1/";
   final Dio _dio ;
 
-  Api(this._dio);
+  ApiService(this._dio);
 
    Future<Map<String , dynamic>> get ({required String endPoint}) async{
   Response response = await  _dio.get('$baseUrl$endPoint');
--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,15 +1,33 @@
+import 'package:demo/features/home/data/models/BookModel.dart';
 import 'package:demo/features/home/domain/entitys/entity.dart';
+
+import '../../../../core/utils/api_service.dart';
 
 abstract class HomeRemoteDataSource {
   Future <List<BookEntity>> fetchFuturesBooks ();
   Future <List<BookEntity>> fetchNewestBooks ();
+  final ApiService apiService ;
+
+  HomeRemoteDataSource(this.apiService);
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
-  @override
-  Future<List<BookEntity>> fetchFuturesBooks() {
+  HomeRemoteDataSourceImpl(super.apiService);
 
-    throw UnimplementedError();
+  @override
+  Future<List<BookEntity>> fetchFuturesBooks() async {
+  var data = await apiService.get(endPoint: "volumes?Filtering=free-ebooks&q=programming");
+  List<BookEntity> books = getBooksList(data);
+  return books ;
+  }
+
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+     List<BookEntity> books = [];
+    for( var book in data['items'] ){
+      books.add(BookModel.fromJson(book));
+    }
+    return books;
   }
 
   @override
